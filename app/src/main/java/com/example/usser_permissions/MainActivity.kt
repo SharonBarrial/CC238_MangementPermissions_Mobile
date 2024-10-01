855f8a9db079cf07546ad7214705e82ea8f80510
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2024-10-01 01:20:31 -0500

    feat: configuration access components (activity main)

--- app/src/main/java/com/example/usser_permissions/MainActivity.kt
+++ app/src/main/java/com/example/usser_permissions/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.usser_permissions
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,5 +21,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //reconocer el controller
+        val btCamera = findViewById<Button>(R.id.btCamera)
+
+        //cuando se haga click en el boton
+        btCamera.setOnClickListener {
+            //verificar permisos de la camara
+            checkCameraPermission()
+        }
+    }
+
+    //metodo para verificar permisos de la camara
+    private fun checkCameraPermission() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        != PackageManager.PERMISSION_GRANTED)
+        {
+            //No tiene permisos
+            Toast.makeText(this, "No tiene permisos", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
